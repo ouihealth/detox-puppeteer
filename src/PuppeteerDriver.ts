74771fd55ad0e9f2070bc835fd3782f3b3d8a1de
Alec Winograd <awinograd@gmail.com>
2020-06-15 18:02:02 -0500

    Add touch indicator and support for disableTouchIndicators launch param

--- src/PuppeteerDriver.ts
+++ src/PuppeteerDriver.ts
@@ -42,6 +42,85 @@ let page: puppeteer.Page | null;
 let urlBlacklist: string[] = [];
 let pendingExport: string | null = null;
 let isRecording = false;
+let disableTouchIndicators = false;
+
+// https://gist.github.com/aslushnikov/94108a4094532c7752135c42e12a00eb
+async function setupTouchIndicators() {
+  await page?.evaluate(() => {
+    if ((window as any).__detox_puppeteer_mouse_pointer) return;
+    (window as any).__detox_puppeteer_mouse_pointer = true;
+    const box = document.createElement('puppeteer-mouse-pointer');
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = `
+      puppeteer-mouse-pointer {
+        pointer-events: none;
+        position: absolute;
+        top: 0;
+        z-index: 10000;
+        left: 0;
+        width: 20px;
+        height: 20px;
+        background: rgba(0,0,0,.4);
+        border: 1px solid white;
+        border-radius: 10px;
+        margin: -10px 0 0 -10px;
+        padding: 0;
+        transition: background .2s, border-radius .2s, border-color .2s;
+      }
+      puppeteer-mouse-pointer.button-1 {
+        transition: none;
+        background: rgba(0,0,0,0.9);
+      }
+      puppeteer-mouse-pointer.button-2 {
+        transition: none;
+        border-color: rgba(0,0,255,0.9);
+      }
+      puppeteer-mouse-pointer.button-3 {
+        transition: none;
+        border-radius: 4px;
+      }
+      puppeteer-mouse-pointer.button-4 {
+        transition: none;
+        border-color: rgba(255,0,0,0.9);
+      }
+      puppeteer-mouse-pointer.button-5 {
+        transition: none;
+        border-color: rgba(0,255,0,0.9);
+      }
+    `;
+    document.head.appendChild(styleElement);
+    document.body.appendChild(box);
+    document.addEventListener(
+      'mousemove',
+      (event) => {
+        box.style.left = event.pageX + 'px';
+        box.style.top = event.pageY + 'px';
+        updateButtons(event.buttons);
+      },
+      true,
+    );
+    document.addEventListener(
+      'mousedown',
+      (event) => {
+        updateButtons(event.buttons);
+        box.classList.add('button-' + event.which);
+      },
+      true,
+    );
+    document.addEventListener(
+      'mouseup',
+      (event) => {
+        updateButtons(event.buttons);
+        box.classList.remove('button-' + event.which);
+      },
+      true,
+    );
+    function updateButtons(buttons) {
+      for (let i = 0; i < 5; i++) box.classList.toggle('button-' + i, !!(buttons & (1 << i)));
+    }
+  });
+}
+
 class PuppeteerTestee {
   configuration: any;
   client: typeof Client;
@@ -431,6 +510,9 @@ class PuppeteerTestee {
 
     await this.client.ws.open();
     this.client.ws.ws.on('message', async (str) => {
+      if (!disableTouchIndicators) {
+        await setupTouchIndicators();
+      }
       // https://github.com/wix/Detox/blob/ca620e760747ade9cb673c28262200b02e8e8a5d/docs/Troubleshooting.Synchronization.md#settimeout-and-setinterval
       // async function setupDetoxTimeouts() {
       //   await page.evaluate(() => {
@@ -748,6 +830,7 @@ class PuppeteerDriver extends DeviceDriverBase {
       launchArgs,
     });
 
+    disableTouchIndicators = launchArgs.disableTouchIndicators;
     const defaultViewport = launchArgs.viewport || this._getDefaultViewport();
     const headless = this._getDeviceOption('headless', false);
     if (!headless && process.env.CI) {
